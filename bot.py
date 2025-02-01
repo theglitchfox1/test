0097fc701570b1d28179137624547fae0f6f9020
Gwant <theglitchfox1@gmail.com>
2025-02-01 13:52:51 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,6 +7,21 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if 'https://' in message.text:
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+           bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+           bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.") 
+        
+
+    
+
+
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
